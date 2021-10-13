a4bb8e9123ee5a748aa4fdc867e7ad5fd257c2ca
nlyunsal <nlyunsal@gmail.com>
2021-10-13 18:33:24 +0300

    Add project files.

---
+++ tc_kimlik_Sorgulama_methodsuz/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace tc_kimlik_Sorgulama_methodsuz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_kontrol = new System.Windows.Forms.Button();
+            this.txt_tcno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_kontrol
+            // 
+            this.btn_kontrol.Location = new System.Drawing.Point(177, 89);
+            this.btn_kontrol.Name = "btn_kontrol";
+            this.btn_kontrol.Size = new System.Drawing.Size(75, 23);
+            this.btn_kontrol.TabIndex = 0;
+            this.btn_kontrol.Text = "Kontrol Et";
+            this.btn_kontrol.UseVisualStyleBackColor = true;
+            this.btn_kontrol.Click += new System.EventHandler(this.btn_kontrol_Click);
+            // 
+            // txt_tcno
+            // 
+            this.txt_tcno.Location = new System.Drawing.Point(177, 41);
+            this.txt_tcno.Name = "txt_tcno";
+            this.txt_tcno.Size = new System.Drawing.Size(177, 20);
+            this.txt_tcno.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tc No Giriniz:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 181);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_tcno);
+            this.Controls.Add(this.btn_kontrol);
+            this.Name = "Form1";
+            this.Text = "Tc No Sorgulama Ekranı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kontrol;
+        private System.Windows.Forms.TextBox txt_tcno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ tc_kimlik_Sorgulama_methodsuz/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tc_kimlik_Sorgulama_methodsuz
+{
+    public partial class Form1 : Form
+    {
+
+        int[] tc = new int[11];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_kontrol_Click(object sender, EventArgs e)
+        {
+            string tckimlikno = txt_tcno.Text;
+
+            if (txt_tcno.Text!=" ")
+            {
+                if (tckimlikno.Length == 11)
+                {
+                    if (long.TryParse(tckimlikno, out long result))
+                    {
+                        for (int i = 0; i < tc.Length; i++)
+                        {
+
+                            tc[i] = int.Parse(tckimlikno.Substring(i, 1));
+                        }
+
+                        if (tc[0] != 0)
+                        {
+
+                            int a = ((tc[0] + tc[2] + tc[4] + tc[6] + tc[8]) * 7) + ((tc[1] + tc[3] + tc[5] + tc[7]) * 9);
+
+                            if (a % 10 == tc[9])
+                            {
+                                int toplam = tc[0] + tc[2] + tc[4] + tc[6] + tc[8] + tc[1] + tc[3] + tc[5] + tc[7] + tc[9];
+
+                                if (toplam % 10 == tc[10])
+                                {
+                                    int toplam2 = tc[0] + tc[2] + tc[4] + tc[6] + tc[8];
+
+                                    if ((toplam2 * 8) % 10 == tc[10])
+                                    {
+                                        MessageBox.Show("Geçerli Tc");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("6.maddeye uymuyor");
+                                    }
+                                }
+                                else
+                                {
+
+                                    MessageBox.Show("5.maddeye uymuyor");
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("4.maddeye uymuyor");
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("tc no sıfırla başlayamaz!");
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("girilen değer doğru formatta değil");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("girilen değer 11 karakter olmalıdır!");
+                }
+            }
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //string tckimlikno = txt_tcno.Text;
+            //if (txt_tcno.Text!=" ")
+            //{
+            //    if (tckimlikno.Length == 11)
+            //    {
+            //        if (int.TryParse(tckimlikno, out int result))
+            //        {
+
+            //            for (int i = 0; i < tc.Length; i++)
+            //            {
+            //                tc[i] = int.Parse(tckimlikno.Substring(i, 1));
+                           
+            //            }
+            //            if (tc[0]!=0)
+            //            {
+            //              if( (((tc[0]+tc[2]+tc[4]+tc[6]+tc[8])*7) + ((tc[0] + tc[2] + tc[4] + tc[6]*9)) ) % 10 == tc[9])
+
+
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("ilk hanesi sıfır olamaz");
+            //            }
+
+
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Lütfen doğru bir format giriniz");
+
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("lütfen bir değer giriniz");
+            //}
+
+
+
+        }
+    }
+}
